094727d8465f9f499dea76e2e0e2bc3c45012947
Nikola Zorić <71042177+Zorrkela@users.noreply.github.com>
2023-04-23 16:23:44 +0200

    Update StationRepository.java

--- src/main/java/com/example/project/Repositories/StationRepository.java
+++ src/main/java/com/example/project/Repositories/StationRepository.java
@@ -1,7 +1,6 @@
 package com.example.project.Repositories;
 
 import com.example.project.Models.StationModel;
-import com.example.project.Models.WagonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
